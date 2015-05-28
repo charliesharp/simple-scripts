d0f5a4ac78031541f5a0a6154130b9e23f911319
csharp <csharp@smoopa.com>
2015-05-27 18:56:55 -0700

    Ensure supervisor process gets restarted if an exception occurs

--- cleanup_prerender.py
+++ cleanup_prerender.py
@@ -2,18 +2,32 @@
 from subprocess import check_call, check_output
 import glob
 import os
+
+def kill_running_phantomjs_processes():
+    running_processes = check_output(['ps', 'auxww']).split('\n')
+    for process_line in running_processes:
+        if 'node_modules/phantomjs' in process_line:
+            process_details = process_line.split()
+            pid = process_details[1]
+            try:
+                check_call(['sudo', 'kill', pid])
+                print 'Killed phantomjs process %s' % pid
+            except:
+                print 'Unable to kill phantomjs process %s (already stopped?)' % pid
+
+def remove_crash_dump_files():
+    num_files = 0
+    for eachfile in glob.iglob('/tmp/*.dmp'):
+        os.remove(eachfile)
+        num_files += 1
+    print 'Removed %s crash dump files' % num_files
+
 check_call(['sudo', 'service', 'supervisor', 'stop'])
-running_processes = check_output(['ps', 'auxww']).split('\n')
-for process_line in running_processes:
-    if 'node_modules/phantomjs' in process_line:
-        process_details = process_line.split()
-        pid = process_details[1]
-        print 'Killing phantomjs process %s' % pid
-        check_call(['sudo', 'kill', pid])
-num_files = 0
-for eachfile in glob.iglob('/tmp/*.dmp'):
-    os.remove(eachfile)
-    num_files += 1
-print 'Removed %s crash dump files' % num_files
-check_call(['sudo', 'service', 'supervisor', 'start'])
+try:
+    kill_running_phantomjs_processes()
+    remove_crash_dump_files()
+except Exception as e:
+    raise e
+finally:
+    check_call(['sudo', 'service', 'supervisor', 'start'])
 print 'Done!'
